b5b25e3eb9e5664dcd01f2db543efbc66b168421
Tony Holmes <tonyholmes@Mac.localdomain>
2025-06-16 21:30:46 -0500

    Fix: use HashRouter for GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 // src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';     // NO “.tsx” extension here
+import { HashRouter } from 'react-router-dom';
+import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
